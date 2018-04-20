0083f8432cde5f2bf89cc7caedc7e3dfdc1e629a
ThePhantomPotatoLord <37915783+ThePhantomPotatoLord@users.noreply.github.com>
2018-04-20 12:49:30 -0500

    Add files via upload

---
+++ Master_Page_Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using POS_WEB_APP.Models;
+
+namespace POS_WEB_APP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create_Transaction()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult View_Reports()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Check_Inventory()
+
+        {
+            return View();
+        }
+
+        public IActionResult Customer_Lookup()
+
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
